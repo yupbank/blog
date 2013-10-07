c0c31ab03c07b604af9aa82382237e68812917ba
airtrack <superking.airtrack@gmail.com>
2013-10-07 19:30:17 +0800

    Using html template.

--- blog.rkt
+++ blog.rkt
@@ -1,6 +1,8 @@
 #lang racket
 (require web-server/servlet
-         web-server/servlet-env)
+         web-server/templates
+         web-server/servlet-env
+         xml)
 
 (define (blog-start req)
   (blog-dispatch req))
@@ -12,20 +14,26 @@
    [else blog-list-posts]))
 
 (define (blog-list-posts req)
-  (response/xexpr
-   `(html (head (title "blog"))
-          (body (h1 "new blog main page")
-                (a ([href "/posts/post-1"])
-                   "post 1")
-                (a ([href "/posts/post-2"])
-                   "post 2")
-                (a ([href "/posts/post-3"])
-                   "post 3")))))
+  (posts-list "blog" (xexpr->string `(body (h2 "My blog")
+                                           (a ([href "/posts/post-1"])
+                                              "post 1")
+                                           (br)
+                                           (a ([href "/posts/post-2"])
+                                              "post 2")
+                                           (br)
+                                           (a ([href "/posts/post-3"])
+                                              "post 3")))))
 
 (define (blog-review-post req blog-name)
   (response/xexpr
    `(html (head (title ,blog-name))
           (body (p ,blog-name)))))
 
+(define (posts-list title body)
+  (response/xexpr
+   (make-cdata #f #f (include-template "index.html"))))
+
 (serve/servlet blog-start
-               #:file-not-found-responder blog-dispatch)
+               #:file-not-found-responder blog-dispatch
+               #:servlet-path "/"
+               #:extra-files-paths (list (current-directory)))
